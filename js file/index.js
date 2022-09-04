b3416781f2229c9282952338eef6c0cf38fb7947
unknown <sadafzillani98@gmail.com>
2022-09-04 09:01:26 +0530

    seventh commit

--- js file/index.js
+++ js file/index.js
@@ -32,36 +32,58 @@ function findCategoryInfo(categoryInfoURL) {
     .then((data) => getData(data));
 }
 function getData(data) {
-  //console.log(data.data[0].author.name);
-  //   data.data.forEach((c) => {
-  //     console.log(c.author.name);
-  //   });
-
+  var indexString = data.data[0].category_id.slice(1, 2);
+  var indexNumber = parseInt(indexString);
+  document.getElementById("numberOfItems").innerText = `${
+    data.data.length
+  } items found for category ${categoryName[indexNumber - 1]}`;
   const newsContainer = document.getElementById("card-group");
   newsContainer.innerText = "";
   data.data.forEach((info) => {
-    console.log(info);
+    //console.log(info._id);
     const newNewsContainer = document.createElement("div");
     newNewsContainer.innerHTML = `
   <div class="card d-flex flex-column justify-content-center align-items-center pt-3 flex-lg-row"> 
-  <img class="img-fluid w-25" src="${info.image_url}" alt="Card image cap" />
+  <img class="img-fluid w-25" src="${info.image_url}" alt="Card image cap"  />
     <div class="card-body">
       <h5 class="card-title">${info.title}</h5>
       <p class="card-text">
         ${info.details.slice(0, 300)}...
       </p>
       <div class="d-flex flex-row justify-content-between">
+        <div class="d-flex flex-row">
+          <div>
+            <img src=${
+              info.author.img
+            } class="rounded-circle" width=50 height=50/>
+          </div>
         <div>
+          <span>${info.author.name}</span> <br>
+          <span> <small>${info.author.published_date}</small></span>
+        </div>
+        </div>
+        <div> <i class="fa-solid fa-eye"></i> ${info.total_view} </div>
         <div>
-        <img src=${info.author.img} class="rounded-circle" width=50 height=50/>
-        </div>
-        <div></div>
-        </div>
-        <div>asd</div>
-        <div>asd</div>
+       <button type="button" onclick="getNewsDetails('${
+         info._id
+       }')" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+       <i  class="fa-solid fa-arrow-right"></i>
+       </button>
+       </div>       
       </div>
     </div>
   </div>`;
     newsContainer.appendChild(newNewsContainer);
   });
+}
+
+function getNewsDetails(id) {
+  const newsURL = "https://openapi.programming-hero.com/api/news/" + id;
+  console.log(newsURL);
+  fetch(newsURL)
+    .then((res) => res.json())
+    .then((data) => getModalBody(data));
+}
+function getModalBody(data) {
+  document.getElementById("newsModalBody").innerText = data.data[0].details;
 }
