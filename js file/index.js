e9f34d93488308e5151f3a4da4ed95e01692b226
unknown <sadafzillani98@gmail.com>
2022-09-04 06:03:59 +0530

    fourth commit

--- js file/index.js
+++ js file/index.js
@@ -23,6 +23,32 @@ function selectClicked(event) {
 function showCategory(clickedCategoryName) {
   var categoryString =
     "0" + (categoryName.indexOf(clickedCategoryName) + 1).toString();
-  console.log(categoryString);
   const clickedCategoryNameURL = `https://openapi.programming-hero.com/api/news/category/${categoryString}`;
+  findCategoryInfo(clickedCategoryNameURL);
+}
+function findCategoryInfo(categoryInfoURL) {
+  fetch(categoryInfoURL)
+    .then((res) => res.json())
+    .then((data) => getData(data));
+}
+function getData(data) {
+  const newsContainer = document.getElementById("card-group");
+  console.log(newsContainer);
+  const newNewsConatainer = document.createElement("div");
+  newNewsConatainer.innerHTML = `
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap" />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">
+        This is a wider card with supporting text below as a natural
+        lead-in to additional content. This content is a little bit
+        longer.
+      </p>
+      <p class="card-text">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </p>
+    </div>
+  </div>`;
+  newsContainer.appendChild(newNewsConatainer);
 }
